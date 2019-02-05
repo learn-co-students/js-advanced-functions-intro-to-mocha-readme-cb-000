ef755dbb93003d8313204ee0d1c2314ae2024133
Matt Trussell <smtrussell@gmail.com>
2019-02-05 04:23:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+/*jshint esversion:6*/
+
+
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return 'Hey!';
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`;
+}
+
+function favoriteIceCream(frozenTreat) {
+  return `I love ${frozenTreat}`;
+}
+
+function shouting(what) {
+  return what.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
